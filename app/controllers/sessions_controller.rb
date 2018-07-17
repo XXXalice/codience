f8d2f0cf3579fb86a720baeaf20eba38975edafe
梅宮 <umemiya@MacBook-Pro.local>
2018-07-17 16:02:53 +0900

    add basic-login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "入力されたユーザー名かパスワードに誤りがあります。"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+    flash[:success] = "ログアウトしました。"
+  end
+
+end
